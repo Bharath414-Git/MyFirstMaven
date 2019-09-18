0af8edfd2c617861459e40f971db191fd7bd35dc
Bharath414-Git <bharath.gadwala1996@gmail.com>
2019-09-18 22:47:54 +0530

    Setup done

--- src/main/java/com/baseclass/BaseClass.java
+++ src/main/java/com/baseclass/BaseClass.java
@@ -1,8 +1,34 @@
 package com.baseclass;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BaseClass {
+	public Properties prop;
+	public WebDriver driver;
+	public BaseClass() throws Exception {
+		try {
+	    prop = new Properties();
+		FileInputStream ip = new FileInputStream("F:\\Java_24_Workspace\\com.artifact\\src\\main\\java\\com\\properties\\config.properties");
+		prop.load(ip);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
 	
-	public BaseClass() {
+	public void Initailization() {
+		System.out.println(prop.getProperty("browser"));
+		if(prop.getProperty("browser").equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver",
+					  "F:\\drivers\\geckodriver.exe");
+			 driver = new FirefoxDriver();
+		}
 	}
 
 }
---
+++ src/test/java/com/testcases/LoginPage_Test.java
@@ -0,0 +1,26 @@
+package com.testcases;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.baseclass.BaseClass;
+
+public class LoginPage_Test extends BaseClass {
+	
+	
+	public LoginPage_Test() throws Exception {
+		super();
+		
+	}
+	
+	@Test
+	public void Method() {
+		Initailization();
+		
+	}
+	
+	
+	
+	
+
+}
